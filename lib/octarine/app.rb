965b08fff8e877c6ff53ec1b35d1ac5327b5679d
matsadler <mat@sourcetagsandcodes.com>
2011-11-25 14:02:39 +0000

    refactor

--- lib/octarine/app.rb
+++ lib/octarine/app.rb
@@ -37,28 +37,23 @@ module Octarine # :nodoc:
       # 
       # Adds block as a handler for path when the request method is PUT.
       
-      [:add, :get, :post, :delete, :put].each do |method|
+      ##
+      # :method: default
+      # :call-seq: default {|request| block }
+      # 
+      # Adds block as a handler for when no path is matched.
+      
+      [:add, :get, :post, :delete, :put, :default].each do |method|
         define_method(method) do |*args, &block|
           (@handlers ||= []) << [method, *args, block]
         end
       end
       
-      # :call-seq: default {|request| block }
-      # 
-      # Adds block as a handler for when no path is matched.
-      # 
-      def default(&block)
-        @default = block
-      end
-      
-      # :call-seq: request_class(klass)
-      # 
       # Set the class of the request object handed to the path handler blocks.
       # Defaults to Octarine::Request.
       # 
-      def request_class(klass=nil)
-        klass ? @request_class = klass : @request_class || Octarine::Request
-      end
+      attr_writer :request_class
+      alias request_class request_class=
       
       # :call-seq: environment -> string
       # 
@@ -69,55 +64,65 @@ module Octarine # :nodoc:
       end
       
       def new(*args) # :nodoc:
-        super.tap do |instance|
-          instance.router = HttpRouter.new
-          @handlers.each do |method, *args|
-            block = args.pop
-            route = instance.router.send(method, *args)
-            route.to do |env|
-              env.merge!("router.route" => route.original_path)
-              response = instance.instance_exec(request_class.new(env), &block)
-              to_rack_response(response)
-            end
-          end
-          instance.router.default(Proc.new do |env|
-            response = instance.instance_exec(request_class.new(env), &@default)
-            to_rack_response(response)
-          end) if @default
-        end
-      end
-      
-      private
-      
-      def to_rack_response(res)
-        if res.respond_to?(:status) && res.respond_to?(:headers) &&
-          res.respond_to?(:body)
-          status, headers, body = res.status, res.headers, res.body
-          [status, headers, body.respond_to?(:each) ? body : [body].compact]
-        elsif res.respond_to?(:to_ary)
-          res.to_ary
-        elsif res.respond_to?(:to_str)
-          [200, {}, [res.to_str]]
-        else
-          [200, {}, res]
+        request_class = @request_class || Octarine::Request
+        handlers = @handlers
+        super.instance_eval do
+          @request_class ||= request_class
+          @router ||= HttpRouter.new
+          handlers.each {|m,*args| register_handler(m, *args[0..-2], &args[-1])}
+          self
         end
       end
       
     end
     
-    attr_accessor :router # :nodoc:
+    extend Forwardable
+    
+    attr_reader :router, :request_class
     
     ##
     # :method: call
     # :call-seq: app.call(env) -> array
     # 
     # Rack-compatible #call method.
-    
-    extend Forwardable
-    def_delegators :router, :url, :call
+    # 
+    def_delegator :router, :call
     
     def self.included(includer) # :nodoc:
       includer.extend(ClassMethods)
+    end
+    
+    private
+    
+    def to_rack_response(res)
+      if res.respond_to?(:status) && res.respond_to?(:headers) &&
+        res.respond_to?(:body)
+        status, headers, body = res.status, res.headers, res.body
+        [status, headers, body.respond_to?(:each) ? body : [body].compact]
+      elsif res.respond_to?(:to_ary)
+        res.to_ary
+      elsif res.respond_to?(:to_str)
+        [200, {}, [res.to_str]]
+      else
+        [200, {}, res]
+      end
+    end
+    
+    def register_handler(method, *args, &block) # :nodoc:
+      return register_default(&block) if method == :default
+      route = router.send(method, *args)
+      route.to do |env|
+        env.merge!("router.route" => route.original_path)
+        request = request_class.new(env)
+        response = instance_exec(request, &block)
+        to_rack_response(response)
+      end
+    end
+    
+    def register_default(&block) # :nodoc:
+      router.default(Proc.new do |env|
+        to_rack_response(instance_exec(request_class.new(env), &block))
+      end)
     end
     
   end
